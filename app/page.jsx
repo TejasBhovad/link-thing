769253427f75434b65f9251b3d3d1702902f008f
Tejas Bhovad <tejasbhovad@gmail.com>
2023-07-02 14:20:53 +0530

    init commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main className="justify-center flex w-full h-full bg-gray-200">
+      <div className="bg-red-200 mobile w-3/4 h-100 max-w-3xl"></div>
+    </main>
+  );
+}
+
+export default Home;
